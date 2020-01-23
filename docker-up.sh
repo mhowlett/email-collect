c3e45bae8a2a1b7e72839dc30083474662a1b0a2
Matthew Howlett <matt.howlett@gmail.com>
2020-01-22 20:03:20 -0800

    localhost -> 127.0.0.1

--- docker-up.sh
+++ docker-up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker run -d --name email-landing \
-    -p localhost:7979:7979 \
+    -p 127.0.0.1:7979:7979 \
     -v /git/prelaunch-landing/append-email:/src/ \
     -v /data/prelaunch-landing:/data/ \
     landing:1 \
