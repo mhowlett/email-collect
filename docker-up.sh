5a059d6f65f6181ad3f5ff1bd2854ecb6d6313a1
Matthew Howlett <matt.howlett@gmail.com>
2020-01-22 20:08:01 -0800

    interactive for testng

--- docker-up.sh
+++ docker-up.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker run -d --name email-landing \
+docker run -it --name email-landing \
     -p 127.0.0.1:7979:7979 \
     -v /git/prelaunch-landing/append-email:/src/ \
     -v /data/prelaunch-landing:/data/ \
     landing:1 \
-    /src/bootstrap.sh
+    bash
